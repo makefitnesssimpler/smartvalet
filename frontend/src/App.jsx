cce51476711019a8bc96cf3802fa86783b60daea
fitness <makefitnesssimpler@gmail.com>
2026-05-01 17:11:22 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useMemo, useRef, useState } from 'react';
-import QRCode from 'qrcode.react';
+import { QRCodeCanvas } from 'qrcode.react';
 
 const API_BASE = 'http://localhost:4000/api/tickets';
 const CUSTOMER_PATH = '/customer-ticket';
@@ -335,8 +335,7 @@ function ValetDashboard() {
                       <tr>
                         <td colSpan={5}>
                           <div className="qr-row">
-                            <QRCode value={getCustomerUrl(t.id)} size={140} />
-                            <img src={getQrImageUrl(t.id)} alt={`QR for ticket ${t.id}`} width={140} height={140} />
+                            <QRCodeCanvas value={getCustomerUrl(t.id)} size={140} />
                             <div className="qr-links">
                               <a href={getCustomerUrl(t.id)} target="_blank" rel="noreferrer">
                                 Open customer ticket page
